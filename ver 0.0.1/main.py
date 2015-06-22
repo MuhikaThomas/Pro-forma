1800e7c50bc379202f1874fbd4091a8ab6623cd2
Muhika Thomas <tommuhyka@hotmail.com>
2015-06-22 23:35:19 +0300

    Day 3, buggs

---
+++ ver 0.0.1/main.py
@@ -0,0 +1,17 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.tabbedpanel import TabbedPanelHeader
+from kivy.uix.scrollview import ScrollView
+from kivy.lang import Builder
+
+class Tabs(TabbedPanel):
+	pass
+
+class Estate(App):
+	def build(self):
+		return Tabs()
+
+if __name__ == '__main__':
+	Estate().run()
