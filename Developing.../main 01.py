d89bb0ecd07275e5238919977f3e0a62ea107b4f
Muhika Thomas <tommuhyka@hotmail.com>
2015-06-23 23:33:49 +0300

    Trying something new in /Developin.../main 01.py

--- Developing.../main 01.py
+++ Developing.../main 01.py
@@ -1,13 +1,17 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.tabbedpanel import TabbedPanelHeader
+from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
 from kivy.lang import Builder
-	
+
 class Tabs(TabbedPanel):
-	pass
+
 
 class Estate(App):
 	def build(self):
--- ver 0.0.1/main.py
+++ ver 0.0.1/main.py
@@ -5,11 +5,6 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.tabbedpanel import TabbedPanelHeader
 from kivy.uix.scrollview import ScrollView
 from kivy.lang import Builder
-
-class controller(ScrollView):
-	def __init__(self, **kwargs):
-		super(Controller, self).__init__(**kwargs)
-        	self.layout_content.bind(minimum_height=self.layout_content.setter('height'))
 	
 class Tabs(TabbedPanel):
 	pass
