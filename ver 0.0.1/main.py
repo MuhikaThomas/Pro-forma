0e472b6e66dc152b3dab4c3aedfb66af5e11a43c
Muhika Thomas <tommuhyka@hotmail.com>
2015-06-25 19:27:50 +0300

    Whoogit add D*/! Finally able to set up the widgets in order

--- ver 0.0.1/main.py
+++ ver 0.0.1/main.py
@@ -1,17 +1,49 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.tabbedpanel import TabbedPanelHeader
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
 from kivy.lang import Builder
-	
-class Tabs(TabbedPanel):
-	pass
 
-class Estate(App):
+Builder.load_string("""
+""")
+
+class Proforma(App):
 	def build(self):
-		return Tabs()
+	#*******ROOTWIDGET*******
+		layout = GridLayout(rows=2)
+	#*******SUB-WIDGETS*******
+		layoutTop = FloatLayout()#SUB-WIDGET-1
+		layoutMid = FloatLayout()#SUB-WIDGET-2
+	#*******CONTENT-OF-SUB-WIDGET-1*******	
+		title = Label(text = 'Pro-Forma App', font_size = '40sp', pos = (0,280))
+	#*******CONTENT-OF-SUB-WIDGET-2*******
+		tp_panel = TabbedPanel()		
+		th_tab1 = TabbedPanelHeader(text = 'Pro-Forma')
+		th_tab1.content =  Label(text= 'Property Name', font_size='20sp')
+		th_tab1.content = TextInput(text='input', font_size=15, halign ='left', valign='middle')
+		th_tab2 = TabbedPanelHeader(text = 'Info. Tab')
+		th_tab3 = TabbedPanelHeader(text = 'Due Deligence')
+		th_tab4 = TabbedPanelHeader(text = 'Saved Reports')
+	#*******CALLING-TABS-TO-tp_panel*******
+		tp_panel.add_widget(th_tab1)
+		tp_panel.add_widget(th_tab2)
+		tp_panel.add_widget(th_tab3)
+		tp_panel.add_widget(th_tab4)	
+	#*******ADDING-CONTENTS-OF-SUB-WIDGETS*******
+		layoutTop.add_widget(title)
+		layoutMid.add_widget(tp_panel)
+	#*******ADDING-CONTENTS-OF-ROOT-WIDGET*******
+		layout.add_widget(layoutTop)
+		layout.add_widget(layoutMid)
+	#*******CALLING-THE-ROOT-WIDGET*******	
+		return layout
 
 if __name__ == '__main__':
-	Estate().run()
+	Proforma().run()
